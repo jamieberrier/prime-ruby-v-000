ea2947b11641a72dfad0726a8b78d330895470af
jamieberrier <berrier.jamie@gmail.com>
2018-07-31 17:34:26 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,11 @@ require 'pry'
 
 #takes in an integer argument and returns a boolean of whether or not that integer is a prime number
 def prime?(number)
-
-
-  end
+  if number >= 2
+     (2..number - 1).all? do |x|
+       number % x != 0
+     end
+   else
+     return false
+   end
 end
