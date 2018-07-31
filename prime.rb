a7075ca4c42228c6a6e2aac91878c11a09e5dfdb
jamieberrier <berrier.jamie@gmail.com>
2018-07-31 17:29:50 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,5 +3,9 @@ require 'pry'
 
 #takes in an integer argument and returns a boolean of whether or not that integer is a prime number
 def prime?(n)
-
+  sieve = []
+  (2..max).each { |i| sieve[i] = i }
+  (2..Math.sqrt(max)).each do |i|
+  (i*i).step(max, i) { |j| sieve[j] = nil } if sieve[i]
+  end
 end
